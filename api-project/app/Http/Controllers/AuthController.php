e3411a07c67f67b25a2a8cb09d134a5c83df5b4e
Andrei Dietrich <169064248+Gesshokuuyu@users.noreply.github.com>
2026-04-29 13:42:35 -0300

    Adiciona controlador de autenticação e rotas para registro, login e logout

---
+++ api-project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' =>  'required|string|max:100',
+            'email' => 'required|email:rfc,dns|unique:users,email',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        $token = $user->createToken('api-token', ['post.read', 'post.create'])->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::attempt($validated)){
+            $user = User::where('email', $validated['email'])->firstOrFail();
+
+            dd($user);
+        }
+
+        return response()->json([
+            'success' => false,
+            'user' => null,
+            'token' => null
+        ], 401);
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+}
